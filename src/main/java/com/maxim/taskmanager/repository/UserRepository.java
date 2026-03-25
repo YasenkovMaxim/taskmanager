c1a17b4b76367219804a0894d410d957da4f8a90
jasen <jasenkoff.mack@yandex.ru>
2026-03-25 20:20:19 +0300

    add Valid

--- src/main/java/com/maxim/taskmanager/repository/UserRepository.java
+++ src/main/java/com/maxim/taskmanager/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.maxim.taskmanager.repository;
 
 
-import com.maxim.taskmanager.model.dto.UserMapper;
 import com.maxim.taskmanager.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
