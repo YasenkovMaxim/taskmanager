893988de0aae1a9c0e2113464d8e7a92dcf40ba1
jasen <jasenkoff.mack@yandex.ru>
2026-04-13 16:27:22 +0300

    add test

--- src/test/java/com/maxim/taskmanager/service/UserServiceTest.java
+++ src/test/java/com/maxim/taskmanager/service/UserServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
@@ -22,6 +23,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class UserServiceTest {
 
     @Mock
